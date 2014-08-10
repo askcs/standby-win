f38d4adbc418b2f073bdee23013db6fd716adbda
Cengiz Ulusoy <culusoy@ask-cs.com>
2014-08-10 19:03:11 +0200

    pages added statically

---
+++ app/pages/groupDetail/groupDetail.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    "use strict";
+
+    var inject = angular.element(document).injector().invoke;
+
+    WinJS.UI.Pages.define("/pages/groupDetail/groupDetail.html", { ready: ready, updateLayout: updateLayout, unload: unload });
+
+    function ready(element, options) {
+        inject(function ($rootScope, $timeout) {
+            $rootScope.$broadcast('ready', { element: element, options: options });
+        });
+    }
+
+    function unload() {
+        inject(function ($rootScope) {
+            $rootScope.$broadcast('unload');
+        });
+    }
+
+    function updateLayout(element, viewState, lastViewState) {
+        inject(function ($rootScope) {
+            $rootScope.$broadcast('updateLayout', { element: element, viewState: viewState, lastViewState: lastViewState });
+        });
+    }
+})();
